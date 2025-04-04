702584be48b779d293b67397b37af26facadcf35
sofuswinther <sfswnthr@gmail.com>
2025-04-04 14:29:31 +0200

    Player character metoder

--- src/PlayerCharacter.java
+++ src/PlayerCharacter.java
@@ -3,24 +3,36 @@ import java.util.ArrayList;
 public class PlayerCharacter implements GameCharacter{
 
     String name = "CharacterName";
-    int health = 0;
-    int attackPower = 0;
+    int health = 100;
+    int attackPower = 15;
     ArrayList<Relic> inventory = new ArrayList<Relic>();
 
     public void attack(GameCharacter enemy) {
 
     }
 
-    public void collectRelics() {
+    public void collectRelic(ArrayList<Relic> inventory, Relic relic) {
+
+        inventory.add(relic);
+        System.out.println(relic + " was added to inventory");
 
     }
 
     public void run() {
+        System.out.println("You ran away successfully");
+        //Menu.metode();
 
     }
 
-    public void potion() {
+    public int potion(int health) {
 
+        health = health+50;
+
+        if(health > 100) {
+            health = 100;
+        }
+
+        return health;
     }
 
     public void dodge() {
